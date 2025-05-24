38883d630c3159d3a17f59634ba32ae3137bc8bf
Max Blancarte <blancarte@gmail.com>
2025-05-24 09:22:58 -0700

    feat: add admin section

---
+++ apps/admin/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { resolve } from "node:path";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@workspace/ui": path.resolve(__dirname, "../../packages/ui/src"),
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
